7f445168ef769547fb329a5bc41d4d122d929ec6
Brekk <brekk@brekkbockrath.com>
2021-01-05 08:33:41 +0100

    modify renderComponents and expose it

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { HORIZONTAL_BREAKPOINTS, VERTICAL_BREAKPOINTS } from 'bodypaint'
-import { render, screen } from '@testing-library/react'
+import {
+  HORIZONTAL_BREAKPOINTS,
+  VERTICAL_BREAKPOINTS
+} from 'bodypaint'
+import { pipe } from 'ramda'
+import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 import {
   renderBreakpoints,
-  renderVerticalBreakpoints
+  renderVerticalBreakpoints,
+  renderComponents,
+  pointsToDirectionalAttributes
 } from './index'
 
 test('renderBreakpoints', () => {
@@ -14,7 +20,7 @@ test('renderBreakpoints', () => {
     two: 200,
     three: 300
   })
-  const {container}= render(<Breakpoints />)
+  const { container } = render(<Breakpoints />)
   expect(container).toMatchSnapshot()
 })
 
@@ -24,18 +30,28 @@ test('renderVerticalBreakpoints', () => {
     two: 200,
     three: 300
   })
-  const {container}= render(<Breakpoints />)
+  const { container } = render(<Breakpoints />)
   expect(container).toMatchSnapshot()
 })
 
 test('bodypaint defaults', () => {
   const Breakpoints = renderBreakpoints(HORIZONTAL_BREAKPOINTS)
   const VBreakpoints = renderVerticalBreakpoints(VERTICAL_BREAKPOINTS)
-  const {container}= render(
+  const { container } = render(
     <>
-    <Breakpoints />
-    <VBreakpoints />
+      <Breakpoints />
+      <VBreakpoints />
     </>
   )
+  expect(container).toMatchSnapshot()
+})
+
+test('renderComponents', () => {
+  const Comp = props => `${JSON.stringify(props, null, 2)}`
+  const Breakpoints = pipe(
+    pointsToDirectionalAttributes('crap'),
+    renderComponents(Comp)
+  )(HORIZONTAL_BREAKPOINTS)
+  const { container } = render(<Breakpoints />)
   expect(container).toMatchSnapshot()
 })
